91b416c2fce18f4c7e23ad19c89c18c302d115ac
LarsBrands <LaanBrands@gmail.com>
2022-04-20 13:25:29 +0200

    ReadMe geupdate op develop branch

--- src/Sagrada/Controller/Main.java
+++ src/Sagrada/Controller/Main.java
@@ -1,14 +1,10 @@
 package Sagrada.Controller;
 
-import javafx.stage.Stage;
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
-
         MyApp app = new MyApp();
-        Stage s = new Stage();
-        app.start(s);
+        app.launch(args);
     }
 
 }
